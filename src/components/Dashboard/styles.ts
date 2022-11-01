5969b57364791b86a1f82de7f41dba05fce11330
Savvas <savaravant@gmail.com>
2022-11-02 01:58:27 +0200

    Add some minor mobile styles changes

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,15 +1,19 @@
-import styled from "styled-components";
-import { styled as styledMUi } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 
-export const Container = styled.div`
-  display: flex;
-  margin: 70px auto;
-  width: 65%;
-  position: relative;
-  box-shadow: 0px 2px 8px 0px rgba(99, 99, 99, 0.2);
-`;
+export const Container = styled('div')(({ theme }) => ({
+  display: 'flex',
+  margin: '70px auto',
+  width: '65%',
+  position: 'relative',
+  boxShadow: '0px 2px 8px 0px rgba(99, 99, 99, 0.2)',
+  [theme.breakpoints.down(768)]: {
+    width: "100%",
+    height: "100%",
+    margin: "0px",
+  }
+}));
 
-export const FormContainer = styledMUi('div')(({ theme }) => ({
+export const FormContainer = styled('div')(({ theme }) => ({
   padding: '20px',
   maxWidth: '50%',
   flex: '50%',
--- src/components/Persons/styles.ts
+++ src/components/Persons/styles.ts
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 
 export const StyledList = styled(List)(({ theme }) => ({
     [theme.breakpoints.down(768)]: {
-      height: "35rem",
+      height: "100vh",
       maxWidth: "25%",
       overflow: "scroll",
       paddingRight: 5
